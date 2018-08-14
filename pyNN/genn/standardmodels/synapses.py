35d87f3442a69d4df2ae5683cc865dea146ea250
Anton Komissarov <anton.v.komissarov@gmail.com>
2018-08-14 14:59:04 +0200

    move native standard model to __init__.py; clean-up

--- pyNN/genn/standardmodels/synapses.py
+++ pyNN/genn/standardmodels/synapses.py
@@ -6,57 +6,20 @@ Standard cells for the GeNN module.
 :license: CeCILL, see LICENSE for details.
 """
 from copy import copy, deepcopy
+from string import Template
 from pyNN.standardmodels import synapses, StandardModelType, build_translations
 from ..simulator import state
 import logging
-import libgenn
+import pygenn
 import GeNNModel
 from ..conversions import convert_to_single, convert_to_array, convert_init_values
+from . import GeNNStandardSynapseType, GeNNDefinitions
 
 logger = logging.getLogger("PyNN")
 
-class GeNNStandardSynapseType(StandardModelType):
-
-    genn_weightUpdate = None
-
-    def translate_dict(self, val_dict):
-        return {self.translations[n]['translated_name'] : convert_to_array(v)
-                for n, v in val_dict.items() if n in self.translations.keys()}
-
-    def get_native_params(self, connections, init_values, param_names):
-        init_values_ = self.default_initial_values.copy()
-        init_values_.update(init_values)
-        native_init_values = self.translate_dict(init_values_)
-        native_params = {}
-        for pn in param_names:
-            if any([hasattr(conn, pn) for conn in connections]):
-                native_params[pn] = []
-                for conn in connections:
-                    if hasattr(conn, pn):
-                        native_params[pn].append(getattr(conn, pn))
-            elif pn in native_init_values.keys():
-                native_params[pn] = native_init_values[pn]
-            else:
-                raise Exception('Variable "{}" not found'.format(pn))
-
-        return native_params
-
-    def get_params(self, connections, init_values):
-        param_names = list(self.genn_weightUpdate.getParamNames())
-
-        # parameters are single-valued in GeNN
-        return convert_to_array(
-                 self.get_native_params(connections,
-                                        init_values,
-                                        param_names))
-
-    def get_vars(self, connections, init_values):
-        var_names = [vnt[0] for vnt in self.genn_weightUpdate.getVars()]
-
-        return convert_init_values(
-                    self.get_native_params(connections,
-                                           init_values,
-                                           var_names))
+class DDTemplate(Template):
+    '''Template string class with the delimiter overridden with double $'''
+    delimiter = '$$'
 
 genn_tsodyksMakram = (
     # definitions
@@ -74,7 +37,7 @@ genn_tsodyksMakram = (
                $(u) = $(U);
             }
             $(y) += $(x) * $(u);
-            $(addtoinSyn) = $(g) * $(x) * $(u);
+            $(addToInSyn, $(g) * $(x) * $(u));
             $(updatelinsyn);
         ''',
         'paramNames' : [
@@ -115,7 +78,7 @@ class StaticSynapse(synapses.StaticSynapse, GeNNStandardSynapseType):
             d = state.dt
         return d
 
-    genn_weightUpdate = libgenn.WeightUpdateModels.StaticPulse()
+    genn_weightUpdate = pygenn.WeightUpdateModels.StaticPulse()
 
 
 class TsodyksMarkramSynapse(synapses.TsodyksMarkramSynapse, GeNNStandardSynapseType):
@@ -149,16 +112,16 @@ genn_stdp = [
         'paramNames' : [],
         'varNameTypes' : [('g', 'scalar')],
 
-        'simCode' : '''
-            $(addtoinSyn) = $(g);
+        'simCode' : DDTemplate('''
+            $(addToInSyn, $(g));
             $(updatelinsyn);
             scalar dt = $(t) - $(sT_post);
-            {0}
-        ''',
-        'learnPostCode' : '''
+            $${TD_CODE}
+        '''),
+        'learnPostCode' : DDTemplate('''
             scalar dt = $(t) - $(sT_pre);
-            {0}
-        ''',
+            $${TD_CODE}
+        '''),
 
         'isPreSpikeTimeRequired' : True,
         'isPostSpikeTimeRequired' : True,
@@ -169,124 +132,6 @@ genn_stdp = [
     )
 ]
 
-genn_add_wd = [
-    # definitions
-    {
-        'paramNames' : [
-            'Wmin',     # td + 1 - Minimum weight
-            'Wmax',     # td + 2 - Maximum weight
-        ],
-
-        'varNameTypes' : [],
-
-        'depressionUpdateCode' : '''
-            scalar newWeight = $(g) - update;
-        ''',
-
-        'potentiationUpdateCode' : '''
-            scalar newWeight = $(g) + update;
-        ''',
-        'boundaryCode' : '''
-            if(newWeight < $(Wmin))
-            {
-                $(g) = $(Wmin);
-            }
-            else if(newWeight > $(Wmax))
-            {
-                $(g) = $(Wmax);
-            }
-            else
-            {
-                $(g) = newWeight;
-            }
-        '''
-    },
-    # translations
-    (
-        ('w_max',     'Wmax'),
-        ('w_min',     'Wmin'),
-    )
-]
-
-genn_mult_wd = deepcopy(genn_add_wd)
-genn_mult_wd[0]['depressionCode'] = '''
-    scalar newWeight = $(g) - $(g) * update;
-'''
-genn_mult_wd[0]['potentiationCode'] = '''
-    scalar newWeight = $(g) + $(g) * update;
-'''
-
-genn_add_p_mult_d_wd = deepcopy(genn_add_wd)
-genn_add_p_mult_d_wd[0]['potentiationCode'] = '''
-    scalar newWeight = $(g) + $(g) * update;
-'''
-
-genn_spikePair_td = (
-    # definitions
-    {
-        'paramNames' : [
-            'tauPlus',  # 0 - Potentiation time constant (ms)
-            'tauMinus', # 1 - Depression time constant (ms)
-            'Aplus',    # 2 - Rate of potentiation
-            'Aminus',   # 3 - Rate of depression
-        ],
-
-        'varNameTypes' : [],
-
-        'simCode' : '''
-            if (dt > 0)
-            {
-                scalar update = $(Aminus) * exp(-dt / $(tau_minus));
-                {0}
-            }
-        ''',
-
-        'learnPostCode' : '''
-            if (dt > 0)
-            {
-                scalar update = $(Aplus) * exp(-dt / $(tauPlus));
-                {0}
-            }
-        ''',
-    },
-    # translations
-    (
-        ('tau_plus',   'tauPlus'),
-        ('tau_minus',  'tauMinus'),
-        ('A_plus',     'Aplus'),
-        ('A_minus',    'Aminus'),
-    )
-)
-
-genn_vogels2011_td = (
-    # definitions
-    {
-        'paramNames' : [
-            'Tau',      # 0 - Plasticity time constant (ms)
-            'Rho',      # 1 - Target rate
-            'Eta',      # 2 - Learning rate
-        ],
-
-        'varNameTypes' : [],
-
-        'simCode' : '''
-             scalar update = $(Eta) * exp(-dt / $(Tau)) - $(Rho);
-             {0}
-        ''',
-
-        'learnPostCode' : '''
-            scalar update = $(Eta) * exp(-dt / $(Tau));
-            {0}
-        ''',
-
-    },
-    # translations
-    (
-        ('tau', 'Tau'),
-        ('eta', 'Eta'),
-        ('rho', 'Rho')
-    )
-)
 
 class STDPMechanism(synapses.STDPMechanism, GeNNStandardSynapseType):
     __doc__ = synapses.STDPMechanism.__doc__
@@ -316,21 +161,17 @@ class STDPMechanism(synapses.STDPMechanism, GeNNStandardSynapseType):
         settings['varNameTypes'].extend(self.timing_dependence.varNameTypes)
         settings['varNameTypes'].extend(self.weight_dependence.varNameTypes)
 
-        settings['simCode'] = settings['simCode'].format(
-                self.timing_dependence.simCode.format(
-                    wd=(self.weight_dependence.depressionUpdateCode +
-                    self.weight_dependence.boundaryCode),
-                    lbrc='{', rbrc='}'
-                ),
-                lbrc='{', rbrc='}'
+        settings['simCode'] = settings['simCode'].substitute(
+                TD_CODE=self.timing_dependence.simCode.substitute(
+                    WD_CODE=self.weight_dependence.depressionUpdateCode +
+                    self.weight_dependence.boundaryCode
+                )
         )
-        settings['learnPostCode'] = settings['learnPostCode'].format(
-                self.timing_dependence.learnPostCode.format(
-                    wd=self.weight_dependence.potentiationUpdateCode +
-                    self.weight_dependence.boundaryCode,
-                    lbrc='{', rbrc='}'
-                ),
-                lbrc='{', rbrc='}'
+        settings['learnPostCode'] = settings['learnPostCode'].substitute(
+                TD_CODE=self.timing_dependence.learnPostCode.substitute(
+                    WD_CODE=self.weight_dependence.potentiationUpdateCode +
+                    self.weight_dependence.boundaryCode
+                )
         )
         self.genn_weightUpdate = GeNNModel.createCustomWeightUpdateClass('STDP',
                 **settings)()
@@ -424,7 +265,6 @@ class GutigWeightDependence(synapses.GutigWeightDependence, WeightDependence):
         ('mu_minus', 'muMinus')
     )
 
-
 class SpikePairRule(synapses.SpikePairRule):
     __doc__ = synapses.SpikePairRule.__doc__
 
@@ -438,21 +278,21 @@ class SpikePairRule(synapses.SpikePairRule):
     varNameTypes = []
 
     # using {.brc} for left{ or right} so that .format() does not freak out
-    simCode = '''
+    simCode = DDTemplate('''
         if (dt > 0)
-        {lbrc}
+        {
             scalar update = $(Aminus) * exp(-dt / $(tauMinus));
-            {wd}
-        {rbrc}
-    '''
+            $${WD_CODE}
+        }
+    ''')
 
-    learnPostCode = '''
+    learnPostCode = DDTemplate('''
         if (dt > 0)
-        {lbrc}
+        {
             scalar update = $(Aplus) * exp(-dt / $(tauPlus));
-            {wd}
-        {rbrc}
-    '''
+            $${WD_CODE}
+        }
+    ''')
 
     translations = build_translations(
         ('tau_plus',   'tauPlus'),
@@ -473,18 +313,19 @@ class Vogels2011Rule(synapses.Vogels2011Rule):
 
     varNameTypes = []
 
-    simCode = '''
+    simCode = DDTemplate('''
          scalar update = $(Eta) * exp(-dt / $(Tau)) - $(Rho);
-         {wd}
-    '''
+         $${WD_CODE}
+    ''')
 
-    learnPostCode = '''
+    learnPostCode = DDTemplate('''
         scalar update = $(Eta) * exp(-dt / $(Tau));
-        {wd}
-    '''
+        $${WD_CODE}
+    ''')
 
     translations = build_translations(
         ('tau', 'Tau'),
         ('eta', 'Eta'),
         ('rho', 'Rho')
     )
+
