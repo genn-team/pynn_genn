5f15dadb93de9368aab22fc1e6f19ee7950caa9a
jamie <neworderofjamie@gmail.com>
2018-11-07 17:53:56 +0000

    Yet another attempt to fix simulation time mess

--- pynn_genn/simulator.py
+++ pynn_genn/simulator.py
@@ -22,26 +22,8 @@ class State(common.control.BaseState):
         self.min_delay = 0
         self.clear()
         self.dt = 0.1
+        self.t = 0.0
         self.num_current_sources = 0
-
-    @property
-    def t( self ):
-        if self._built:
-            return self.model.t
-        else:
-            return 0.0
-
-    @t.setter
-    def t(self, t):
-        if self._built:
-            self.model.t = t
-            self.model.timestep = int(round(t / self.dt))
-        elif t != 0.0:
-            assert False
-
-    @t.setter
-    def dt( self, _dt ):
-        self.model.dT = _dt
 
     @property
     def dt( self ):
@@ -71,11 +53,20 @@ class State(common.control.BaseState):
             for rec in self.recorders:
                 rec.init_data_views()
 
+        # Synchronise model with our timestep
+        self.model.t = self.t
+        self.model.timestep = int(round(self.t / self.dt))
+
         # Simulate
         self.running = True
-        while True:
+        while self.t < tstop:
             # Get time at start of step (this is correct timestamp for recorded data)
             timestep = self.model.timestep
+
+            # Also update our t from the GeNN timestep
+            # **NOTE** GeNN steps time at end of timestep and PyNN expects opposite
+            # **NOTE** GeNN often using 32-bit float timesteps which fail a lot of tests
+            self.t = timestep * float(self.dt)
 
             # Advance model time
             self.model.step_time()
@@ -83,10 +74,6 @@ class State(common.control.BaseState):
             # Record any variables being recorded
             for rec in self.recorders:
                 rec._record_vars(timestep)
-
-            # If we've passed stop time, stop
-            if self.t > tstop:
-                break
 
     def clear(self):
         self.model = GeNNModel('float', 'GeNNModel')
