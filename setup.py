fd9a8c27f015492817a7d9d99ab11e7e5ab83014
jamie <neworderofjamie@gmail.com>
2018-11-12 12:35:16 +0000

    There seems to be issues with numpy 1.15 and neo 0.6 - locked numpy requirement at 1.14 for now

--- setup.py
+++ setup.py
@@ -34,6 +34,6 @@ setup(
     # Requirements
     # **NOTE** PyNN really should be requiring lazyarray itself but it (0.9.2) doesn't seem to
     install_requires=["pynn>=0.9, <1.0", "pygenn", "lazyarray>=0.3, < 0.4", 
-                      "sentinel", "neo>=0.6, <0.7", "numpy>1.6", "six"],
+                      "sentinel", "neo>=0.6, <0.7", "numpy>1.6, < 1.15", "six"],
     zip_safe=False,  # Partly for performance reasons
 )
