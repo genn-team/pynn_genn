8f10e2376fabef272ad0cb8c571a6e145930b6da
alishdipani <alish.dipani@gmail.com>
2020-03-30 05:07:20 +0530

    Fixing #43

--- pynn_genn/projections.py
+++ pynn_genn/projections.py
@@ -23,7 +23,7 @@ from .contexts import ContextMixin
 # Tuple type used to store details of GeNN sub-projections
 SubProjection = namedtuple("SubProjection",
                            ["genn_label", "pre_pop", "post_pop",
-                            "pre_slice", "post_slice", "syn_pop"])
+                            "pre_slice", "post_slice", "syn_pop", "wum_params"])
 
 
 '''
@@ -179,7 +179,7 @@ class Projection(common.Projection, ContextMixin):
         for sub_pop in self._sub_projections:
             # Loop through names and pull variables
             for n in names:
-                if n != "presynaptic_index" and n != "postsynaptic_index":
+                if n != "presynaptic_index" and n != "postsynaptic_index" and n in sub_pop.syn_pop.vars:
                     genn_model.pull_var_from_device(sub_pop.genn_label, n)
 
         # If projection is sparse
@@ -200,7 +200,10 @@ class Projection(common.Projection, ContextMixin):
                     # Get slice of variable matrix
                     sub_var = var[sub.pre_slice, sub.post_slice]
 
-                    sub_var[sub_pre_inds,sub_post_inds] = sub.syn_pop.get_var_values(n)
+                    if n in sub_pop.syn_pop.vars:
+                        sub_var[sub_pre_inds,sub_post_inds] = sub.syn_pop.get_var_values(n)
+                    else:
+                        sub_var[sub_pre_inds,sub_post_inds] = sub.wum_params[n]
 
                 # Add variable to list
                 variables.append(var)
@@ -453,4 +456,4 @@ class Projection(common.Projection, ContextMixin):
 
             self._sub_projections.append(
                 SubProjection(genn_label, pre_pop, post_pop,
-                              pre_slice, post_slice, syn_pop))
+                              pre_slice, post_slice, syn_pop, wum_params))
